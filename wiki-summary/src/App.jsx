531594f0e2f71769b6781c33d86bf35aeaf7af55
Breyten Cowen <breyten.cowen@gmail.com>
2024-12-21 00:40:34 -0500

    made app.jsx, title and subtitle added

---
+++ wiki-summary/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+    return(
+        <>
+            <div id="title">
+                <h1>Wiki-Summary</h1>
+                <h2>By: Breyten Cowen</h2>
+            </div>
+        </>
+    )
+}
+
+export default App
